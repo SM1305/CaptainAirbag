de1e69c0c2156593ae8c2e25db8708c0d2f463f7
Tom <tom.gibbs@hotmail.co.uk>
2018-08-13 13:16:31 +0100

    Basic movement, scene, interaction
    .

---
+++ Cpt.Airbag/Assets/Scripts/Airbags.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Airbags : MonoBehaviour
+{
+    private GameObject playerEmpty;
+    private PlayerController playerController;
+
+    public float maxInflation;
+    public float deflateSpeed;
+
+    public bool contact;
+    public bool deflate;
+
+    private Vector3 originalScale;
+    private Vector3 tempScale;
+
+    void Start ()
+    {
+        playerEmpty = GameObject.FindGameObjectWithTag("PlayerEmpty");
+        playerController = playerEmpty.GetComponent<PlayerController>();
+
+        Vector3 originalScale = transform.localScale;
+    }
+	
+
+	void Update ()
+    {
+        if (contact)
+        {
+            Debug.Log("contact true");
+            playerController.velocity = playerController.startVelocity;
+
+            //inflate airbag
+            tempScale = transform.localScale;
+            tempScale = tempScale * maxInflation;
+            transform.localScale = tempScale;
+
+            //leave condition
+            contact = false;
+            deflate = true;
+        }
+
+        if (deflate)
+        {
+            //gradually deflate airbag
+            transform.localScale = Vector3.Lerp(transform.localScale, originalScale, Time.deltaTime * deflateSpeed);
+            Debug.Log("deflating true");
+
+            if (transform.localScale == originalScale)
+            {
+                deflate = false;
+            }
+        }
+    }
+
+    
+    //on collision, toggle bool to begin inflation, destroy gameobject col
+    void OnCollisionEnter(Collision col)
+    {
+        if (!col.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Contact made with: " + col.gameObject.tag);
+            Destroy(col.gameObject);
+            contact = true;
+        }
+    }
+}
---
+++ Cpt.Airbag/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,146 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody player;
+    [Header("Acceleration")]
+    public float velocity;
+    public float startVelocity;
+    public float acceleration;
+    public float maxVelocity;
+    public float minVelocity = 1f;
+    public float brake;
+    private float startDrag;
+    [Header("Horizontal Movement")]
+    public float sideSpeed;
+    //public float horizontalSpeed;
+    //public float horizontalAcceleration;
+    [Header("Vertical Movement")]
+    public float jumpHeight;
+    public float crouchHeight;
+    public bool isGrounded;
+
+    private Vector3 originalScale;
+    private Vector3 tempScale;
+    private Vector3 tempVelocity;
+
+
+	void Start ()
+    {
+        player = GetComponent<Rigidbody>();
+        startDrag = player.drag;
+        velocity = startVelocity;
+        Vector3 originalScale = transform.localScale;
+    }
+
+    void Update()
+    {
+        Debug.DrawRay(transform.position, -Vector3.up, Color.green);
+        IsGrounded();
+
+        if ((Input.GetKey(KeyCode.S)))
+        {
+            player.drag = brake;
+        }
+        else
+        {
+            player.drag = startDrag;
+            velocity = Mathf.Lerp(velocity, maxVelocity, 0.1f * Time.deltaTime);
+        }
+        //keep velocity within bounds
+        if (velocity >= maxVelocity)
+        {
+            velocity = maxVelocity;
+        }
+
+        //jump
+        if ((Input.GetKey(KeyCode.UpArrow) && isGrounded))
+        {
+            player.AddForce(transform.up * jumpHeight);
+        }
+        //duck
+        if ((Input.GetKey(KeyCode.DownArrow)))
+        {
+            tempScale = transform.localScale;
+            tempScale.y = tempScale.y * crouchHeight;
+            transform.localScale = tempScale;
+        }
+        else
+        {
+            //transform.localScale = originalScale;
+        }
+
+        //left
+        if (Input.GetKey(KeyCode.A))
+        {
+            //player.AddForce((transform.right * -sideSpeed) * Time.deltaTime, ForceMode.Impulse);
+            player.AddForce((transform.right * (-velocity /6)) * Time.deltaTime, ForceMode.Impulse);
+
+            //transform.Translate(Vector3.right * -sideSpeed);
+
+            //horizontalSpeed -= horizontalAcceleration * Time.deltaTime;
+
+            //tempVelocity = player.velocity;
+            //tempVelocity.x -= horizontalAcceleration * Time.deltaTime;
+            //player.velocity = tempVelocity;
+        }
+        //right
+        else if (Input.GetKey(KeyCode.D))
+        {
+            //player.AddForce((transform.right * sideSpeed) * Time.deltaTime, ForceMode.Impulse);
+            player.AddForce((transform.right * (velocity /6)) * Time.deltaTime, ForceMode.Impulse);
+
+            //transform.Translate(Vector3.right * sideSpeed);
+
+            //horizontalSpeed += horizontalAcceleration * Time.deltaTime;
+
+            //tempVelocity = player.velocity;
+            //tempVelocity.x += horizontalAcceleration * Time.deltaTime;
+            //player.velocity = tempVelocity;
+        }
+
+
+
+        //player acceleration
+        player.AddForce(transform.forward * velocity * Time.deltaTime);
+    }
+
+    void FixedUpdate ()
+    {
+        ////player acceleration
+        //player.AddForce(transform.forward * velocity * Time.deltaTime);
+
+        ////horizontal movement
+        //if (horizontalSpeed < 0)
+        //{
+        //    player.AddForce(new Vector3(-1, 0, 0) * (-horizontalSpeed * Time.deltaTime));
+        //}
+        //if (horizontalSpeed > 0)
+        //{
+        //    player.AddForce(transform.right * (horizontalSpeed * Time.deltaTime));
+        //}
+    }
+
+
+    bool IsGrounded()
+    {
+        if (!isGrounded)
+        {
+            return true;
+        }
+        else
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, -Vector3.up, out hit, 0.5f))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
